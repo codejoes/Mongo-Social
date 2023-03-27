67becef2e58de82523acd994a522b76aef1f7119
codejoes <114871953+codejoes@users.noreply.github.com>
2023-03-27 07:27:14 -0400

    adding router and pseudocode to routes

---
+++ routes/api/postRoutes.js
@@ -0,0 +1,15 @@
+const router = require('express').Router();
+
+//GET USER
+
+//GET SINGLE USER
+
+//CREATE USER
+
+//UPDATE USER
+
+//DELETE USER
+
+//ADD FRIEND
+
+//DELETE FRIEND
---
+++ routes/api/userRoutes.js
@@ -0,0 +1,15 @@
+const router = require('express').Router();
+
+//GET POSTS
+
+//GET SINGLE POST
+
+//CREATE POST
+
+//UPDATE POST
+
+//DELETE POST
+
+//CREATE COMMENT
+
+//DELETE COMMENT
